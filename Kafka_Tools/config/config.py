dd97729163292ee6f8e5859fcff8b3de0064d540
yitzchakdamen <yitzchakdamen@gmail.com>
2025-08-27 11:26:56 +0300

    Add logging configuration and info messages to config.py

--- Kafka_Tools/config/config.py
+++ Kafka_Tools/config/config.py
@@ -1,6 +1,12 @@
 import os
 from dotenv import load_dotenv
+import logging
 
 load_dotenv()
+logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
 
 KAFKA_BOOTSTRAP_SERVERS = os.getenv("KAFKA_BOOTSTRAP_SERVERS","")
+
+
+logging.info("Configuration Loaded:")
+logging.info(f"KAFKA_BOOTSTRAP_SERVERS: {KAFKA_BOOTSTRAP_SERVERS}")
