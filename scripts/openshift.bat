522779f6f9338e27c9235d0f55a89601f903d505
yitzchakdamen <yitzchakdamen@gmail.com>
2025-08-28 16:58:04 +0300

    Working on openshift

--- scripts/openshift.bat
+++ scripts/openshift.bat
@@ -5,6 +5,13 @@ cd C:\Users\isaac\source\repos\Malicious_Text_Feature_Engineering_System_V2\k8s
 
 oc login --token=sha256~wIkZMHNN3ulD36e2clrOc37yqDTmRdiYQ3GhZ4PszYQ --server=https://api.rm3.7wse.p1.openshiftapps.com:6443
 
+oc delete svc --all
+oc delete pvc --all
+oc delete deployments --all
+oc delete pods --all
+oc delete statefulsets --all
+oc delete routes --all
+
 oc apply -f Kafka-Broker.yaml
 oc apply -f MongoDB.yaml
 
